9e20b846efe94250c770a30c33db7219d8f18021
Yusuke Toba <toba@ws.cs.kobe-u.ac.jp>
2015-11-06 14:55:18 +0900

    前回作業終了段階

--- src/jp/kobe_u/cspiral/cloudgarden/devices/DevicesController.java
+++ src/jp/kobe_u/cspiral/cloudgarden/devices/DevicesController.java
@@ -62,6 +62,56 @@ public class DevicesController {
 		}
 	}
 
+	public static String getState(){
+		//温度湿度を取得
+		InterfaceKitPhidget ik;
+		double humidity;
+		double temperature;
+		try {
+			ik = new InterfaceKitPhidget();
+			ik.openAny();
+			System.out.println("waiting for InterfaceKit attachment...");
+			ik.waitForAttachment();
+
+			humidity = (ik.getSensorValue(0) * 0.1906) - 40.2;
+			temperature = (ik.getSensorValue(1) * 0.22222) - 61.11;
+			ik.close();
+
+			//{temperature: 27, humidity: 80}
+		} catch (PhidgetException e) {
+			e.printStackTrace();
+			return "{\"status\": \"failure\", \"message\": \"センサーからデータを取得できませんでした．\"}";
+		}
+
+		//画像を取得
+		Webcam webcam = null;
+		webcam = Webcam.getDefault();
+		if (webcam != null) {
+			System.out.println("Webcam : " + webcam.getName());
+			webcam.open();
+			BufferedImage image = webcam.getImage();
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+			try {
+				ImageIO.write(image, "png", baos);
+				baos.flush();
+				byte[] imageInByte = baos.toByteArray();
+				baos.close();
+				byte[] b64 = Base64.encodeBase64(imageInByte);
+				String imageInMime = new String(b64);
+				System.out.println(imageInMime);
+				return "{\"status\": \"success\", \"temperature\": \""+ temperature +"\", \"humidity\":\"" + humidity + "\", \"image\": \""+ imageInMime +"\"}";
+			} catch (IOException e) {
+				e.printStackTrace();
+				return "{\"status\": \"failure\", \"message\": \"画像データの変換に失敗しました\"}";
+			}
+		} else {
+			System.out.println("Failed: Webcam Not Found Error");
+			return "{\"status\": \"failure\", \"message\": \"Webカメラが見つかりませんでした．\"}";
+		}
+	}
+
+
 	//TODO: 水をあげるメソッド
 
 }
--- src/jp/kobe_u/cspiral/cloudgarden/devices/JaxAdapter.java
+++ src/jp/kobe_u/cspiral/cloudgarden/devices/JaxAdapter.java
@@ -9,6 +9,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+
 @Path("/api")
 public class JaxAdapter {
 
@@ -46,7 +49,7 @@ public class JaxAdapter {
 		//wsc.sendMessage(message);
 		RestClient client = new RestClient("superuser", "superuser");
 
-		String uri = "http://54.199.139.148/cloud_garden_server/api/submitTemperatureAndHumidiy";
+		String uri = "http://54.199.139.148/cloud_garden_server/api/submitTemperatureAndH";
 		client.post(uri, message, String.class, MediaType.APPLICATION_JSON_TYPE);
 
 		return Response.status(200).entity("<return>"+ message +"</return>").build();
@@ -67,5 +70,27 @@ public class JaxAdapter {
 		return Response.status(200).entity("<return>"+ message +"</return>").build();
 	}
 
+	@GET
+	@Produces({MediaType.APPLICATION_XML})
+	@Path("/updateState")
+	public Response updateState() {
+		System.out.println("in /updateState");
+		Client c = Client.create();
+
+		//接続するアドレス
+        WebResource resource = c.resource(
+                "http://54.199.139.148/cloud_garden_server/api/updateState"
+                );
+		String jsonText = DevicesController.getState();
+        String response = resource.type(MediaType.APPLICATION_JSON_TYPE).
+                post(String.class, jsonText);
+        System.out.println("----------------------------");
+        System.out.println("★パラメータ");
+        System.out.println(jsonText);
+        System.out.println("★結果");
+        System.out.println(response);
+
+		return Response.status(200).entity("<return>"+ jsonText +"</return>").build();
+	}
 
 }
